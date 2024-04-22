fb88cd584f2f9bfcc48514101a343eac1406587e
Mark kevin Dango <themcry@gmail.com>
2024-04-22 22:20:31 +0800

    pushinginamers

--- pages-admin/book.php
+++ pages-admin/book.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Guest</title>
+    <title>Booking</title>
     <link rel="shortcut icon" type="image/x-icon" href="../images/logo1.jpg" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
