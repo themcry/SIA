13a06798105a5fc7042cffcd162cae6c56701c50
Mark kevin Dango <themcry@gmail.com>
2024-05-05 22:44:45 +0800

    pusher ako

--- pages-admin/Dashboard.php
+++ pages-admin/Dashboard.php
@@ -79,7 +79,7 @@
             </ul>
             <div class="sidebar-footer">
             <a href="#" data-toggle="modal" data-target="#myModal" class="sidebar-link" >
-                        <i href="#" class="lni lni-cog" onclick="openNav()"></i>
+                        <i class="lni lni-cog" onclick="openNav()"></i>
                         <div id="mySidebar" class="sidebar">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
   <button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#changePasswordModal">Change Password</button>
