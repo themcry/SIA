561d0a7c84ce803905616f72cd8dd6a8531cd5e8
Mark kevin Dango <themcry@gmail.com>
2024-05-17 22:15:31 +0800

    push

--- pages-admin/guest.php
+++ pages-admin/guest.php
@@ -1,27 +1,34 @@
 <?php
 session_start(); 
-
+require_once('../classes/db_connection.php');
 if (!isset($_SESSION['id'])) {
-  header('Location: ../index.php');
-  exit();
+    header('Location: ../index.php');
+    exit();
 }
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Guest</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard</title>
+    
+    <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="../images/logo1.jpg" />
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-        <link rel="stylesheet" href="../assets/css/1.css">
-    </head>
-    <body>
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="../assets/css/1.css">
+    
+    <!-- LineIcons -->
+    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet">
+    
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined">
+</head>
+<body>
     <div class="wrapper">
         <aside id="sidebar">
             <div class="d-flex">
@@ -33,16 +40,15 @@ if (!isset($_SESSION['id'])) {
                 </div>
             </div>
             <ul class="sidebar-nav">
-                <li class="sidebar-item" class="nav-link active">
-                    <a href="../pages-admin/Dashboard.php" class="sidebar-link">
+                <li class="sidebar-item">
+                    <a href="../pages-admin/Dashboard.php" class="sidebar-link active">
                         <i class="lni lni-grid-alt"></i>
                         <span>Dashboard</span>
                     </a>
                 </li>
                 <li class="sidebar-item">
-                    <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
-                        data-bs-target="#front" aria-expanded="false" aria-controls="front">
-                        <i class="material-symbols-outlined"> desk </i>
+                    <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse" data-bs-target="#front" aria-expanded="false">
+                        <i class="material-symbols-outlined">desk</i>
                         <span>Front-Desk</span>
                     </a>
                     <ul id="front" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
@@ -55,8 +61,7 @@ if (!isset($_SESSION['id'])) {
                     </ul>
                 </li>
                 <li class="sidebar-item">
-                    <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
-                        data-bs-target="#back" aria-expanded="false" aria-controls="back">
+                    <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse" data-bs-target="#back" aria-expanded="false">
                         <i class="material-symbols-outlined">support_agent</i>
                         <span>Back-Office</span>
                     </a>
@@ -70,10 +75,9 @@ if (!isset($_SESSION['id'])) {
                     </ul>
                 </li>
                 <li class="sidebar-item">
-                    <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
-                        data-bs-target="#inventory" aria-expanded="false" aria-controls="inventory">
+                    <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse" data-bs-target="#inventory" aria-expanded="false">
                         <i class="material-symbols-outlined">inventory</i>
-                        <span>inventory</span>
+                        <span>Inventory</span>
                     </a>
                     <ul id="inventory" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
                         <li class="sidebar-item">
@@ -81,30 +85,20 @@ if (!isset($_SESSION['id'])) {
                         </li>
                     </ul>
                 </li>
-                
             </ul>
             <div class="sidebar-footer">
-            <a href="#" data-toggle="modal" data-target="#myModal" class="sidebar-link" >
-                        <i class="lni lni-cog" onclick="openNav()"></i>
-                        <span>Settings</span>
-                        <div id="mySidebar" class="sidebar">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#manageAccountsModal">Manage Accounts</button>
-<!-- The Modal -->
-
-</div>
-
-<a href="#" class="sidebar-link" data-toggle="modal" data-target="#logoutModal">
-    <i class="lni lni-exit"></i>
-    <span>Logout</span>
-</a>
+                <a href="#" class="sidebar-link" data-bs-toggle="modal" data-bs-target="#manageAccountsModal">
+                    <i class="lni lni-cog"></i>
+                    <span>Settings</span>
+                </a>
+                <a href="#" class="sidebar-link" data-bs-toggle="modal" data-bs-target="#logoutModal">
+                    <i class="lni lni-exit"></i>
+                    <span>Logout</span>
+                </a>
             </div>
         </aside>
 
-
-    
-  
-      <main  style="margin-top: 68px;">
+        <main  style="margin-top: 68px;">
         <div class="container-fluid p-3">
           <div class="card bg-transparent border-0" style="font-size: 14px;">
             <div class="card-body">
@@ -181,198 +175,41 @@ if (!isset($_SESSION['id'])) {
 </svg></button></button>&nbsp;<button type="button" class="btn btn-info"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
   <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
   <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-</svg></button><td>
+</svg></button></td>
     </tr>
   </tbody>
 </table>
       </main>
 
-<!-- The Logout Modal -->
-<div class="modal fade" id="logoutModal">
-  <div class="modal-dialog">
-    <div class="modal-content">
-
-      <!-- Modal Header -->
-      <div class="modal-header">
-        <h4 class="modal-title">Confirm Logout</h4>
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-      </div>
-
-      <!-- Modal Body -->
-      <div class="modal-body">
-        <p>Are you sure you want to logout?</p>
-      </div>
-
-      <!-- Modal Footer -->
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-        <a href="../functions/logout.php" class="btn btn-primary">Logout</a>
-      </div>
-
-    </div>
-  </div>
-</div>
-<!-- The Modal -->
-<div class="modal fade" id="manageAccountsModal" tabindex="-1" aria-labelledby="manageAccountsModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="manageAccountsModalLabel">Manage Accounts</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-            <form method="post" action="../functions/add_account.php">
-                <form id="addAccountForm" style="display: inline-flex; gap: 10px; flex-wrap: wrap;">
-                    <div class="form-group">
-                        <label for="username">Username:</label>
-                        <input type="text" class="form-control" id="username" name="username" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password:</label>
-                        <div class="input-group">
-                        <input type="password" class="form-control" id="password" name="password" required>
-                        <span class="input-group-text bg-white border-start-0" id="togglePassword"><i class="fas fa-eye-slash"></i></span>
-                    </div>
-</div>
-                    <button type="submit" class="btn btn-success">Add Account</button>
-                </form>
-
-
-               <!-- List of existing accounts -->
-<h5 class="mt-4">Existing Accounts:</h5>
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Username</th>
-            <th scope="col">Actions</th>
-        </tr>
-    </thead>
-    <tbody id="accountList">
-        <?php
-        require_once('../classes/db_connection.php');
-
-        $sql = "SELECT id, username FROM accounts";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo "<th scope='row'>" . $row["id"] . "</th>";
-                echo "<td>" . $row["username"] . "</td>";
-                echo "<td>";
-                echo "<button type='button' class='btn btn-primary edit-btn' id='editButton' data-bs-toggle='modal' data-bs-target='#editAccountModal'>";
-                echo "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
-                        <path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/>
-                        <path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z'/>
-                    </svg>";
-                echo "</button>";                
-                echo "</button>";
-                echo "<button type='button' class='btn btn-danger delete-btn'>";
-                echo "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'>
-                        <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0'/>
-                    </svg>";
-                echo "</button>";
-                echo "</td>";
-                echo "</tr>";
-            }
-        } else {
-            echo "<tr><td colspan='3'>No accounts found</td></tr>";
-        }
- $result->close();
-
- $conn->close();
- ?>
-</tbody>
-</table>
-            </div>
-        </div>
-    </div>
-</div>
-      </div>
-      </div>
-
-<!-- Edit Modal -->
-<div class="modal fade" id="editAccountModal" tabindex="-1" aria-labelledby="editAccountModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="editAccountModalLabel">Edit Account</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      <form method="POST" action="../functions/account.php">
-        <form id="editAccountForm">
-          <input type="hidden" id="editAccountId">
-          <div class="mb-3">
-            <label for="editUsername" class="form-label">Username</label>
-            <input type="text" class="form-control" id="editUsername" required>
-          </div>
-          <div class="mb-3">
-            <label for="editPassword" class="form-label">Password</label>
-            <input type="password" class="form-control" id="editPassword" required>
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary" id="saveChangesBtn">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+    <?php include('modals-for-admin.php'); ?>
+    <!-- jQuery (required for Bootstrap) -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     
-
-<!-- Delete Modal -->
-
-<div class="modal fade" id="deleteAccountModal" tabindex="-1" aria-labelledby="deleteAccountModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-            <form method="POST" action="../functions/delete-account.php">
-                <h5 class="modal-title" id="deleteAccountModalLabel">Delete Account</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p>Are you sure you want to delete this account?</p>
-                <form id="deleteAccountForm">
-                    <input type="hidden" id="delete-id" name="id">
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-
-    </div>
-  </div>
-</div>
-
-
-
-<script>
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-}
-</script>
-            
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
+    <!-- Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz4fnFO9gybBogGzBa9P30+Y65nT3oB4JUUIsdRRAKkIVkIbo4rxQpEwE5" crossorigin="anonymous"></script>
+    
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyfIhG6j6KbU5l5U07ikChcb6ILm1fM92lw68i3EnfF5Nq3o5xgQAK+" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+    <!-- Custom JS -->
     <script src="../assets/js/1.js"></script>
-    </body>
-    </html>
+
+    <script>
+
+        function openNav() {
+            document.getElementById("mySidebar").style.width = "250px";
+        }
+
+        function closeNav() {
+            document.getElementById("mySidebar").style.width = "0";
+        }
+        function deleteAccount(id) {
+            window.location.href = "../functions/delete-account.php?id="+id;
+        }
+
+
+    </script>
+</body>
+</html>
